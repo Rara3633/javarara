863cbb10698945b55530d75fc1be9851ba51c459
rara3633 <syahirahazizan97@gmail.com>
2024-09-19 15:13:20 +0800

    Second day

---
+++ Calories.java
@@ -0,0 +1,51 @@
+public class Calories{ 
+        
+        }
+
+        public static void(String[] args){
+            int cal = 1000;
+            int incal = 100;
+            int day = 1;
+
+            switch (dayoffweek) {
+                case 1;
+                dayName = "Monday";
+                break;
+            
+                case 2;
+                dayName = "Tuesday";
+                break;
+            
+                case 3;
+                dayName = "Wednesday";
+                break;
+                
+                case 4;
+                dayName = "Thursday";
+                break;
+            
+                case 5;
+                dayName = "Friday";
+                break;
+            
+                case 6;
+                dayName = "Saturday";
+                break;
+                
+                case 7;
+                dayName = "Sunday";
+                break;
+            
+                defult;
+                dayName = "Invalid Value";
+                break;
+            }
+
+            int cal = 100;
+
+        while(cal = 100){ 
+            System.out.printIn(cal);
+            counter++;
+              
+        }
+            }
